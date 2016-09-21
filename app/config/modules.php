db54f573c8363628d4d2e8c3b0eb5d649089898f
Falaleev Maxim <max@studio107.ru>
2016-09-21 17:51:07 +0300

    up

---
+++ app/config/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'Admin' => [
+        'class' => '\Modules\Admin\AdminModule'
+    ],
+    'Auth' => [
+        'class' => '\Modules\Auth\AuthModule'
+    ],
+    'Core' => [
+        'class' => '\Modules\Core\CoreModule'
+    ],
+    'Example' => [
+        'class' => '\Modules\Example\ExampleModule'
+    ],
+    'User' => [
+        'class' => '\Modules\User\UserModule'
+    ],
+];
