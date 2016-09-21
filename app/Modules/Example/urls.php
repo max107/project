1d1bc8aa6d58b8159eeb7b12e72626475b7de73c
Maksim Falaleev <max@studio107.ru>
2016-09-21 22:56:25 +0300

    add acl and middleware route test

--- app/Modules/Example/urls.php
+++ app/Modules/Example/urls.php
@@ -1,10 +1,30 @@
 <?php
 
+use Mindy\Middleware\ResponseTimeMiddleware;
+use Mindy\Permissions\Rule;
 use Modules\Example\Controllers\ExampleController;
 
 return [
     [
         'route' => '/',
-        'callback' => [ExampleController::class, 'getIndex']
+        'callback' => [ExampleController::class, 'getIndex'],
+    ],
+    [
+        'route' => '/test/acl/denied',
+        'callback' => [ExampleController::class, 'getIndex'],
+        'params' => [
+            'rules' => [
+                ['class' => Rule::class, 'users' => '@']
+            ]
+        ]
+    ],
+    [
+        'route' => '/test/middleware',
+        'callback' => [ExampleController::class, 'getIndex'],
+        'params' => [
+            'middleware' => [
+                'response_time' => ['class' => ResponseTimeMiddleware::class]
+            ]
+        ]
     ],
 ];
--- app/config/middleware.php
+++ app/config/middleware.php
@@ -4,7 +4,4 @@ return [
     'csrf' => [
         'class' => \Mindy\Middleware\CsrfMiddleware::class
     ],
-    'response_time' => [
-        'class' => \Mindy\Middleware\ResponseTimeMiddleware::class
-    ]
 ];
